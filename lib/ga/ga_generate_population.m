2f73034fbd32df3ad93913379767bd1c43eeb776
Chris Choi <chutsu@gmail.com>
2016-11-17 10:27:00 -0500

    Remove matlab code

--- lib/ga/ga_generate_population.m
+++
@@ -1,9 +0,0 @@
-function p = ga_generate_popoulation(bitstring_length, population_size)
-    p = zeros(1, bitstring_length, population_size);
-
-    for i = 1:population_size
-        for j = 1:bitstring_length
-            p(1, j, i) = rand > 0.5;
-        end
-    end
-end
