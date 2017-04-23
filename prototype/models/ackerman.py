d511720baf574db6c6a3e7d1356c6075353b361a
Chris Choi <chutsu@gmail.com>
2017-04-23 00:43:51 -0400

    Update

---
+++ prototype/models/ackerman.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from math import cos
+from math import sin
+from math import tan
+
+import numpy as np
+
+
+def ackerman_model(x, u, L, dt):
+    g1 = x[0] + u[0] * cos(x[2]) * dt
+    g2 = x[1] + u[0] * sin(x[2]) * dt
+    g3 = x[2] + ((u[0] * tan(x[2])) / L) * dt
+
+    return np.array([g1, g2, g3])
--- prototype/utils/math.py
+++ prototype/utils/math.py
@@ -2,6 +2,9 @@
 from math import pi
 from math import cos
 from math import sin
+from math import atan2
+from math import asin
+from math import fmod
 
 import numpy as np
 
@@ -102,3 +105,169 @@ def euler2rot(euler, euler_seq):
         R33 = cos(phi) * cos(theta)
 
     return np.array([[R11, R12, R13], [R21, R22, R23], [R31, R32, R33]])
+
+
+def euler2quat(alpha, beta, gamma, euler_seq):
+    c1 = cos(alpha / 2.0)
+    c2 = cos(beta / 2.0)
+    c3 = cos(gamma / 2.0)
+    s1 = sin(alpha / 2.0)
+    s2 = sin(beta / 2.0)
+    s3 = sin(gamma / 2.0)
+
+    if euler_seq == 123:
+        # euler 1-2-3 to quaternion
+        w = c1 * c2 * c3 - s1 * s2 * s3
+        x = s1 * c2 * c3 + c1 * s2 * s3
+        y = c1 * s2 * c3 - s1 * c2 * s3
+        z = c1 * c2 * s3 + s1 * s2 * c3
+        return [w, x, y, z]
+
+    elif euler_seq == "321":
+        # euler 3-2-1 to quaternion
+        w = c1 * c2 * c3 + s1 * s2 * s3
+        x = s1 * c2 * c3 - c1 * s2 * s3
+        y = c1 * s2 * c3 + s1 * c2 * s3
+        z = c1 * c2 * s3 - s1 * s2 * c3
+        return [w, x, y, z]
+
+    else:
+        print("Error! Invalid euler sequence [{0}]".format(euler_seq))
+
+
+def quat2euler(q, euler_seq):
+    qw = q[0]
+    qx = q[1]
+    qy = q[2]
+    qz = q[3]
+
+    qw2 = pow(qw, 2)
+    qx2 = pow(qx, 2)
+    qy2 = pow(qy, 2)
+    qz2 = pow(qz, 2)
+
+    if euler_seq == 123:
+        phi = atan2(2 * (qz * qw - qx * qy), (qw2 + qx2 - qy2 - qz2))
+        theta = asin(2 * (qx * qz + qy * qw))
+        psi = atan2(2 * (qx * qw - qy * qz), (qw2 - qx2 - qy2 + qz2))
+
+    elif euler_seq == 321:
+        phi = atan2(2 * (qx * qw + qz * qy), (qw2 - qx2 - qy2 + qz2))
+        theta = asin(2 * (qy * qw - qx * qz))
+        psi = atan2(2 * (qx * qy + qz * qw), (qw2 + qx2 - qy2 - qz2))
+
+    return [phi, theta, psi]
+
+
+def quat2rot(q):
+    qw = q[0]
+    qx = q[1]
+    qy = q[2]
+    qz = q[3]
+
+    # qw2 = pow(qw, 2)
+    qx2 = pow(qx, 2)
+    qy2 = pow(qy, 2)
+    qz2 = pow(qz, 2)
+
+    # inhomogeneous form
+    R11 = 1 - 2 * qy2 - 2 * qz2
+    R12 = 2 * qx * qy + 2 * qz * qw
+    R13 = 2 * qx * qz - 2 * qy * qw
+
+    R21 = 2 * qx * qy - 2 * qz * qw
+    R22 = 1 - 2 * qx2 - 2 * qz2
+    R23 = 2 * qy * qz + 2 * qx * qw
+
+    R31 = 2 * qx * qz + 2 * qy * qw
+    R32 = 2 * qy * qz - 2 * qx * qw
+    R33 = 1 - 2 * qx2 - 2 * qy2
+
+    # # homogeneous form
+    # R11 = qx2 + qx2 - qy2 - qz2
+    # R12 = 2 * (qx * qy - qw * qz)
+    # R13 = 2 * (qw * qy + qx * qz)
+
+    # R21 = 2 * (qx * qy + qw * qz)
+    # R22 = qw2 - qx2 + qy2 - qz2
+    # R23 = 2 * (qy * qz - qw * qx)
+
+    # R31 = 2 * (qx * qz - qw * qy)
+    # R32 = 2 * (qw * qx + qy * qz)
+    # R33 = qw2 - qx2 - qy2 + qz2
+
+    return np.array([[R11, R12, R13],
+                    [R21, R22, R23],
+                    [R31, R32, R33]])
+
+
+def enu2nwu(enu):
+    # ENU frame:  (x - right, y - forward, z - up)
+    # NWU frame:  (x - forward, y - left, z - up)
+    nwu = [0, 0, 0]
+    nwu[0] = enu[1]
+    nwu[1] = -enu[0]
+    nwu[2] = enu[2]
+    return nwu
+
+
+def cf2nwu(cf):
+    # camera frame:  (x - right, y - down, z - forward)
+    # NWU frame:  (x - forward, y - left, z - up)
+    nwu = [0, 0, 0]
+    nwu[0] = cf[2]
+    nwu[1] = -cf[0]
+    nwu[2] = -cf[1]
+    return nwu
+
+
+def cf2enu(cf):
+    # camera frame:  (x - right, y - down, z - forward)
+    # ENU frame:  (x - right, y - forward, z - up)
+    enu = [0, 0, 0]
+    enu[0] = cf[0]
+    enu[1] = cf[2]
+    enu[2] = -cf[1]
+    return enu
+
+
+def ned2enu(ned):
+    # NED frame:  (x - forward, y - right, z - down)
+    # ENU frame:  (x - right, y - forward, z - up)
+    enu = [0, 0, 0]
+    enu[0] = ned[1]
+    enu[1] = ned[0]
+    enu[2] = -ned[2]
+    return enu
+
+
+def nwu2enu(nwu):
+    # NWU frame:  (x - forward, y - left, z - up)
+    # ENU frame:  (x - right, y - forward, z - up)
+    enu = [0, 0, 0]
+    enu[0] = -nwu[1]
+    enu[1] = nwu[0]
+    enu[2] = nwu[2]
+    return enu
+
+
+def nwu2edn(nwu):
+    # NWU frame:  (x - forward, y - left, z - up)
+    # EDN frame:  (x - right, y - down, z - forward)
+    edn = [0, 0, 0]
+    edn[0] = -nwu[1]
+    edn[1] = -nwu[2]
+    edn[2] = nwu[0]
+    return edn
+
+
+def wrapTo180(euler_angle):
+    return fmod((euler_angle + 180.0), 360.0) - 180.0
+
+
+def wrapTo360(euler_angle):
+    if euler_angle > 0.0:
+        return fmod(euler_angle, 360.0)
+    else:
+        euler_angle += 360.0
+        return fmod(euler_angle, 360.0)
